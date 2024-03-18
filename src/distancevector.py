7fb1fa0e1d32d89dbc356cfa134d0b58107a76ed
LeonardoKamino <leo.kamino@gmail.com>
2024-03-17 21:56:16 -0700

    Update router to have id instead of name

--- src/distancevector.py
+++ src/distancevector.py
@@ -3,16 +3,16 @@ import sys
 data_to_write = "This is the data from distance-vector."
 
 class Router:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, id):
+        self.id = id
         self.neighbors = {}
         self.routing_table = {}
     
     def add_neighbor(self, neighbor, cost):
-        self.neighbors[neighbor.name] =  cost
+        self.neighbors[neighbor.id] =  cost
     
     def update_routing_table(self, destination, next_hop, cost):
-        self.routing_table[destination.name] = (next_hop.name, cost)
+        self.routing_table[destination.id] = (next_hop.id, cost)
 
 class Network:
     def __init__(self, topology_file):
@@ -20,22 +20,22 @@ class Network:
         self.initialize_topology(topology_file)
     
     def add_router(self, router):
-        self.routers[router.name] = router
+        self.routers[router.id] = router
     
     def update_routing_table(self, router, destination, next_hop, cost):
-        self.routers[router.name].update_routing_table(destination, next_hop, cost)
+        self.routers[router.id].update_routing_table(destination, next_hop, cost)
 
     
-    def add_link(self, router_name1, router_name2, cost):
-        if(router_name1 not in self.routers):
-            router1 = Router(router_name1)
+    def add_link(self, router_id1, router_id2, cost):
+        if(router_id1 not in self.routers.keys()):
+            router1 = Router(router_id1)
             self.add_router(router1)
-        if(router_name2 not in self.routers):
-            router2 = Router(router_name2)
+        if(router_id2 not in self.routers.keys()):
+            router2 = Router(router_id2)
             self.add_router(router2)
         
-        router1 = self.routers[router_name1]
-        router2 = self.routers[router_name2]
+        router1 = self.routers[router_id1]
+        router2 = self.routers[router_id2]
 
         router1.add_neighbor(router2, cost)
         router2.add_neighbor(router1, cost)
@@ -44,15 +44,20 @@ class Network:
         with open(topology_file, 'r') as file:
             for line in file:
                 router1, router2, cost = line.split()
-                self.add_link(router1, router2, int(cost))
+                self.add_link(int(router1), int(router2), int(cost))
+
+    def initialize_routing_table(self):
+        # Everynode know their neighbors
+        for router in self.routers.values():
+            for neighbor in router.neighbors.keys():
+                router.update_routing_table(self.routers[neighbor], self.routers[neighbor], router.neighbors[neighbor])
     
     def print_network(self):
         for router in self.routers:
-            transformed_dict = {str(key): value for key, value in self.routers[router].neighbors.items()}
-            
             print(f"Router: {router}")
-            print(f"Neighbors: {transformed_dict}")
+            print(f"Neighbors: {self.routers[router].neighbors}")
             print(f"Routing Table: {self.routers[router].routing_table}")
+
 
 # Open file and create if it doesn't exist
 with open('output.txt', 'w') as file:
@@ -76,7 +81,7 @@ def main():
     topology_file, message_file, changes_file, output_file  = args
 
     network = Network(topology_file)
-    network.update_routing_table(network.routers['1'], network.routers['2'], network.routers['2'], 1)
+    network.initialize_routing_table()
     network.print_network()
     pass
 
