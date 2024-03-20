b8207cdf0087e400bf77aaf9503008bacf6c7b26
Maddy Paulson <madeline1paulson@gmail.com>
2024-03-19 19:42:47 -0700

    Update make.sh
    Co-authored-by: Leonardo Kamino Barros <LeonardoKamino@users.noreply.github.com>

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+##!/bin/bash
 
 # Check if Python is installed
 if command -v python &> /dev/null; then
@@ -11,13 +11,16 @@ else
 fi
 
 # Upgrade pip
-pip install --upgrade pip
+echo "Upgrading pip..."
+$python_cmd -m pip install --upgrade pip
+if [ $? -ne 0 ]; then
+    echo "Failed to upgrade pip. Please check your Python environment."
+    exit 1
+fi
 
 # Install required Python packages
 echo "Installing required Python packages..."
-pip install  pyinstaller
-
-# Check if installations were successful
+$python_cmd -m pip install pyinstaller
 if [ $? -ne 0 ]; then
     echo "Failed to install required Python packages. Please check your Python environment."
     exit 1
@@ -32,7 +35,11 @@ for file in "${source_files[@]}"; do
     src_file="src/${file}.py"
     if [ -f "$src_file" ]; then
         echo "Creating binary executable for $src_file..."
-        pyinstaller --onefile "$src_file"
+        $python_cmd -m PyInstaller --onefile "$src_file"
+        if [ $? -ne 0 ]; then
+            echo "Failed to create binary executable for $src_file. Please check your Python environment."
+            exit 1
+        fi
     else
         echo "Source file $src_file not found."
         exit 1
@@ -41,6 +48,13 @@ done
 
 echo "Binary executables created successfully."
 
+# Remove the build folder and .spec files
+echo "Cleaning up..."
+rm -rf build
+for file in "${source_files[@]}"; do
+    rm -rf "${file}.spec"
+    echo "Removed ${file}.spec"
+done
 
 
 # Remove the build folder
