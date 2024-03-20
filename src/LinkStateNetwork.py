bf7f5548ad0427b29eecc999d3096f2e7efb0a96
LeonardoKamino <leo.kamino@gmail.com>
2024-03-19 20:36:04 -0700

    Doxygen files for LSR

--- src/LinkStateNetwork.py
+++ src/LinkStateNetwork.py
@@ -2,7 +2,18 @@ from Network import Network
 from LinkStateRouter import LinkStateRouter 
 
 class LinkStateNetwork(Network):
+    """
+    Represents a network using the Link State routing algorithm.
+    """
+
     def __init__(self, topology_file, output_file):
+        """
+        Initializes a LinkStateNetwork object.
+
+        Args:
+            topology_file (str): The file path of the topology file.
+            output_file (str): The file path to write the output.
+        """
         super().__init__(topology_file, output_file)
 
     def add_router(self, router_id):
@@ -12,30 +23,33 @@ class LinkStateNetwork(Network):
         Args:
             router_id (int): The ID of the router to add.
         """
-        router = LinkStateRouter(router_id, self)
+        router = LinkStateRouter(router_id, self.routers)
         self.routers[router.id] = router
 
     def distribute_all_lsp(self):
-        # send the lsp to all other routers in the network
-        for  router in self.routers.values():
-            router.generate_lsp(self)
-    
-
-    # def apply_ls_all_routers(self):
-    #     for router_id, router in self.routers.items():
-    #         router.update_routing_table_dijkstra()           
-            
-    def apply_changes_and_output(self, changes_file, message_file):
-        
-        self.distribute_all_lsp()
-        
+        """
+        Distributes the Link State Packets (LSP) from all routers in the network.
+        """
         for router in self.routers.values():
-            router.update_routing_table_dijkstra() 
-            
+            router.generate_lsp(self)
+
+    def apply_changes_and_output(self, changes_file, message_file):
+        """
+        Applies changes to the network and outputs the topology and messages.
+
+        Args:
+            changes_file (str): The file path of the changes file.
+            message_file (str): The file path to write the messages.
+        """
+        self.distribute_all_lsp()
+
+        for router in self.routers.values():
+            router.update_routing_table_dijkstra()
+
         self.topology_output()
         self.send_messages(message_file)
+
         with open(changes_file, 'r') as changes_file:
-            
             for line in changes_file:
                 router_id1, router_id2, cost = line.split()
                 self.process_change(int(router_id1), int(router_id2), int(cost))
@@ -46,6 +60,7 @@ class LinkStateNetwork(Network):
                 router_2.generate_lsp(self)
 
                 for router in self.routers.values():
-                    router.update_routing_table_dijkstra() 
+                    router.update_routing_table_dijkstra()
+
                 self.topology_output()
                 self.send_messages(message_file)
--- src/LinkStateRouter.py
+++ src/LinkStateRouter.py
@@ -2,20 +2,47 @@ from Router import Router
 from utilities import INFINITY
 import heapq
 
+
 class LinkStateRouter(Router):
-    def __init__(self, id, network):
+    """
+    This class represents a Link State Router.
+
+    It inherits from the Router class and implements the Link State Routing algorithm.
+    """
+
+    def __init__(self, id, network_routers):
+        """
+        Initializes a LinkStateRouter object.
+
+        Args:
+            id (int): The ID of the router.
+            network_routers (dict): A dictionary representing the network routers, where the keys are the router IDs and the values are the router objects.
+        """
         super().__init__(id)
         self.lsp_sequence_number = 0
         self.network_topology = {}
-        self.sequence_number_tracker = {router_id: 0 for router_id in network.routers}
-        self.network = network
+        self.sequence_number_tracker = {router_id: 0 for router_id in network_routers.keys()}
+        self.network_routers = network_routers
     
     def generate_lsp(self, network):
+        """
+        Generates a Link State Packet (LSP) and distributes it to all other routers in the network.
+
+        Args:
+            network (Network): The network object representing the network topology.
+        """
         self.lsp_sequence_number += 1
         lsp =  {'id': self.id, 'sequence': self.lsp_sequence_number, 'neighbors': self.neighbors}
         self.distribute_lsp(lsp, network)
     
     def process_lsp(self, lsp, network):
+        """
+        Processes a received Link State Packet (LSP) and updates the network topology if necessary.
+
+        Args:
+            lsp (dict): The received LSP.
+            network (Network): The network object representing the network topology.
+        """
         if lsp['id'] not in self.sequence_number_tracker.keys() or lsp['sequence'] > self.sequence_number_tracker[lsp['id']]:
             self.sequence_number_tracker[lsp['id']] = lsp['sequence']
             self.network_topology[lsp['id']] = lsp['neighbors']
@@ -24,12 +51,27 @@ class LinkStateRouter(Router):
 
     
     def distribute_lsp(self, lsp, network):
+        """
+        Distributes a Link State Packet (LSP) to all other routers in the network.
+
+        Args:
+            lsp (dict): The LSP to be distributed.
+            network (Network): The network object representing the network topology.
+        """
         # send the lsp to all other routers in the network
         for router2_id in self.neighbors.keys():
                 router2 = network.routers[router2_id]
                 router2.process_lsp(lsp, network)
 
     def ls_algorithm(self):
+        """
+        Implements the Link State algorithm to calculate the shortest paths and next hops.
+
+        Returns:
+            tuple: A tuple containing two dictionaries - shortest_paths and next_hops.
+                shortest_paths: A dictionary mapping each node to its shortest path cost from the current router.
+                next_hops: A dictionary mapping each node to its next hop router ID.
+        """
         shortest_paths = {node: INFINITY for node in self.network_topology}
         previous_nodes = {node: None for node in self.network_topology}
         next_hops = {node: None for node in self.network_topology}
@@ -49,6 +91,7 @@ class LinkStateRouter(Router):
                     shortest_paths[neighbor] = distance
                     previous_nodes[neighbor] = current_node
                     heapq.heappush(pq, (distance, neighbor))
+        
         # reconstruct shortest path in order to find the next hop
         for node in self.network_topology:
             if node == self.id:
@@ -68,20 +111,23 @@ class LinkStateRouter(Router):
         return shortest_paths, next_hops
         
     def update_routing_table_dijkstra(self):
-        # print(f"network topology: {self.network_topology}\n")
-        # print(f"router id: {self.id}\n")
-        
+        """
+        Updates the routing table using Dijkstra's algorithm.
+
+        This method calculates the shortest paths and next hops using the Link State algorithm,
+        and updates the routing table accordingly.
+        """
         shortest_paths, next_hops = self.ls_algorithm()
         self.routing_table = {}
         print(f"Shortest paths: ", shortest_paths)
         print(f"Next hops: ", next_hops)
         
         for destination_id, cost in shortest_paths.items():
-            destination_router = self.network.routers[destination_id]
+            destination_router = self.network_routers[destination_id]
             next_hop_id = next_hops[destination_id]
             
             if next_hop_id not in [None, INFINITY] :
-                next_hop_router = self.network.routers[next_hop_id]
+                next_hop_router = self.network_routers[next_hop_id]
             else:
                 next_hop_router = None
                 
