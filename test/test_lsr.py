7cf5ce43cc8003b2585c6ab2fd236fb389eb70b5
LeonardoKamino <leo.kamino@gmail.com>
2024-03-22 15:58:02 -0700

    Update comments for unit test to fit doxygen

--- test/test_lsr.py
+++ test/test_lsr.py
@@ -18,23 +18,21 @@ from LinkStateRouter import LinkStateRouter
 ## @{
 
 class TestLinkState(unittest.TestCase):
+    ## @brief Test case to verify the functionality of the distribute_all_lsp method.
+    #
+    # This test case creates a LinkStateNetwork object, representing a fully connected network and calls the distribute_all_lsp method.
+    # It then checks that all nodes have knowledge about all other nodes in the network.
+    #
+    # Test Steps:
+    # 1. Create a LinkStateNetwork object with the given topology and output file paths.
+    # 2. Call the distribute_all_lsp method to cdistribute knowledge in network.
+    # 3. Check the nodes have correct knowledge of network.
+    #
+    # Expected Results:
+    # - All network topologies are equal.
+    # @test Verifies the functionality of routers in networking distributing knowledge in network in a connected graph.
+
     def test_distribute_all_lsp_connected_graph(self):
-        """
-        \test
-        Test case to verify the functionality of the distribute_all_lsp method.
-
-        This test case creates a LinkStateNetwork object, representing a fully connected network and calls the distribute_all_lsp method.
-        It then checks that all nodes have knowledge about all other nodes in the network.
-
-        Test Steps:
-        1. Create a LinkStateNetwork object with the given topology and output file paths.
-        2. Call the distribute_all_lsp method to cdistribute knowledge in network.
-        3. Check the nodes have correct knowledge of network.
-
-        Expected Results:
-        - All network topologies are equal.
-        """
-
         topology_path = Path(__file__).resolve().parent / "testfiles/topology_connected.txt"
         output_path = Path(__file__).resolve().parent / "testfiles/outputs/lsr/output_distribute_all_lsp_connected.txt"
 
@@ -46,23 +44,23 @@ class TestLinkState(unittest.TestCase):
         self.assertDictEqual(network.routers[1].network_topology, network.routers[2].network_topology)
         self.assertDictEqual(network.routers[2].network_topology, network.routers[3].network_topology)
         self.assertDictEqual(network.routers[3].network_topology, network.routers[4].network_topology)
-
+     
+    ## @brief Test case to verify the functionality of the distribute_all_lsp method in  a disconnected graphs.
+    #
+    # This test case creates a LinkStateNetwork object, representing a disconnected network and calls the distribute_all_lsp method.
+    # It then checks that  nodes have knowledge about all other nodes in their own network.
+    #
+    # Test Steps:
+    # 1. Create a LinkStateNetwork object with the given topology and output file paths.
+    # 2. Call the distribute_all_lsp method to cdistribute knowledge in network.
+    # 3. Check the nodes have correct knowledge of network.
+    #
+    # Expected Results:
+    # - Nodes in same network have the same network topology.
+    # - Nodes in different networks have different network topologies.
+    # @test Verifies the functionality of routers in networking distributing knowledge in network in a disconnected graph.
     def test_distribute_all_lsp_disconnected_graph(self):
-        """
-        Test case to verify the functionality of the distribute_all_lsp method.
-
-        This test case creates a LinkStateNetwork object, representing a disconnected network and calls the distribute_all_lsp method.
-        It then checks that  nodes have knowledge about all other nodes in their own network.
-
-        Test Steps:
-        1. Create a LinkStateNetwork object with the given topology and output file paths.
-        2. Call the distribute_all_lsp method to cdistribute knowledge in network.
-        3. Check the nodes have correct knowledge of network.
-
-        Expected Results:
-        - Nodes in same network have the same network topology.
-        - Nodes in different networks have different network topologies.
-        """
+       
 
         topology_path = Path(__file__).resolve().parent / "testfiles/topology_disconnected.txt"
         output_path = Path(__file__).resolve().parent / "testfiles/outputs/lsr/output_distribute_all_lsp_disconnected.txt"
@@ -76,30 +74,30 @@ class TestLinkState(unittest.TestCase):
         self.assertDictEqual(network.routers[3].network_topology, network.routers[4].network_topology)
         self.assertNotEqual(network.routers[1].network_topology, network.routers[3].network_topology)
         self.assertEqual(network.routers[1].network_topology, network.routers[2].network_topology)
-    
+     
+    ## @brief Test case for the tie breaking in the Link State Routing.
+    #
+    # This test case verifies the functionality of the tie breaking in the Link State Routing.
+    # It creates a LinkStateNetwork object, runs the distribute_all_lsp() method, and checks the routing tables.
+    #
+    # The test checks that the path with the lowest lexigraphuc is chosen in case of a time.
+    # The testfile topology_tie_break.txt contains the following topology with all edges equal to 1:
+    #     3 - 4 - 5 - 6
+    #         \    /
+    #         2 
+    # Therefore routing table from 3 to 6 should have 2 as next hop. Since the path 3-4-5-6 and 3-2-5-6 have the same cost, the path 3-2-5-6 should be chosen as 2 < 4.
+    #
+    # Test Steps:
+    # 1. Create a LinkStateNetwork object.
+    # 2. Call the distribute_all_lsp method to spread info between router.
+    # 3. Call the update_routing_table_dijkstra method to update the routing table of router 3.
+    # 4. Check the routing table of the router 3.
+    #
+    # Expected Results:
+    # - The routing table router 3 has next hop as 2, not 4.
+    #@test Verifies the functionality of the tie breaking in the Link State Routing when 2 paths has same cost.
     def test_tie_break(self):
-        """
-        Test case for the tie breaking in the Link State Routing.
-
-        This test case verifies the functionality of the tie breaking in the Link State Routing.
-        It creates a LinkStateNetwork object, runs the distribute_all_lsp() method, and checks the routing tables.
-
-        The test checks that the path with the lowest lexigraphuc is chosen in case of a time.
-        The testfile topology_tie_break.txt contains the following topology with all edges equal to 1:
-            3 - 4 - 5 - 6
-              \    /
-                2 
-        Therefore routing table from 3 to 6 should have 2 as next hop. Since the path 3-4-5-6 and 3-2-5-6 have the same cost, the path 3-2-5-6 should be chosen as 2 < 4.
-
-        Test Steps:
-        1. Create a LinkStateNetwork object.
-        2. Call the distribute_all_lsp method to spread info between router.
-        3. Call the update_routing_table_dijkstra method to update the routing table of router 3.
-        4. Check the routing table of the router 3.
-
-        Expected Results:
-        - The routing table router 3 has next hop as 2, not 4.
-        """
+       
         topology_path = Path(__file__).resolve().parent / "testfiles/topology_tie_break.txt"
         output_path = Path(__file__).resolve().parent / "testfiles/outputs/lsr/output_tie_break.txt"
         network = LinkStateNetwork(str(topology_path), str(output_path))
@@ -108,35 +106,32 @@ class TestLinkState(unittest.TestCase):
         network.routers[3].update_routing_table_dijkstra()
         self.assertEqual(network.routers[3].routing_table[6], (2, 3))
 
-    def test_tie_break_2(self):
-        """
-        Test case for the tie breaking in the Link State Routing.
-
-        This test case verifies the functionality of the tie breaking in the Link State Routing after changes.
-        It creates a LinkStateNetwork object, runs the distribute_all_lsp() method,apply changes, and checks the routing tables.
-
-        The test checks that the lowest lexigraphic path is selected.
-        The testfile topology_tie_break_2.txt contains the following topology (the number of lines is the cost of the edge):
-            1 - 4 - 12 -- 9
-                \        /
-                  5     11 
-        And the changes_tie_break_2.txt contains the following changes:
-            5 11 1
-
-        Therefore routing table from 4 to 9 should have 5 as next hop. Since the path 4-5-11-9 and 4-12-9 have the same cost, the path 4-5-11-9 should be chosen as 11 < 12.
-
-        Test Steps:
-        1. Create a LinkStateNetwork object.
-        2. Call the distribute_all_lsp method to spread info between router.
-        3. Call the update_routing_table_dijkstra method to update the routing table of router 4.
-        4. Check the routing table of the router 4.
-        5. Apply the changes.
-        
-        
-
-        Expected Results:
-        - The routing table router 3 has next hop as 2, not 4.
-        """
+    ## @brief Test case for the tie breaking in the Link State Routing.
+    #
+    # This test case verifies the functionality of the tie breaking in the Link State Routing after changes.
+    # It creates a LinkStateNetwork object, runs the distribute_all_lsp() method,apply changes, and checks the routing tables.
+    #
+    # The test checks that the lowest lexigraphic path is selected.
+    # The testfile topology_tie_break_2.txt contains the following topology (the number of lines is the cost of the edge):
+    #     1 - 4 - 12 -- 9
+    #         \        /
+    #           5     11 
+    # And the changes_tie_break_2.txt contains the following changes:
+    #     5 11 1
+    #
+    # Therefore routing table from 4 to 9 should have 5 as next hop. Since the path 4-5-11-9 and 4-12-9 have the same cost, the path 4-5-11-9 should be chosen as 11 < 12.
+    #
+    # Test Steps:
+    # 1. Create a LinkStateNetwork object.
+    # 2. Call the distribute_all_lsp method to spread info between router.
+    # 3. Call the update_routing_table_dijkstra method to update the routing table of router 4.
+    # 4. Check the routing table of the router 4.
+    # 5. Apply the changes.
+    #
+    # Expected Results:
+    # - The routing table router 3 has next hop as 2, not 4.
+    #@test Verifies the functionality of updating topology and the tie breaking in the Link State Routing when new route has same cost as old path.
+    def test_tie_break_after_changes(self):
         topology_path = Path(__file__).resolve().parent / "testfiles/topology_tie_break_2.txt"
         output_path = Path(__file__).resolve().parent / "testfiles/outputs/lsr/output_tie_break_2.txt"
         network = LinkStateNetwork(str(topology_path), str(output_path))
