b0605fbc8bf942db22a8f090ea9ec0fcaf462214
LeonardoKamino <leo.kamino@gmail.com>
2024-03-22 14:54:04 -0700

    Update usage section

--- doxygen/html/search/all_11.js
+++ doxygen/html/search/all_11.js
@@ -2,9 +2,10 @@ var searchData=
 [
   ['send_5fmessage_0',['send_message',['../class_network_1_1_network.html#a9718930526ed5a32f879426583518698',1,'Network::Network']]],
   ['send_5fmessages_1',['send_messages',['../class_network_1_1_network.html#a0b43a563348499988cde6b418d5d3243',1,'Network::Network']]],
-  ['shared_20effort_2',['Shared Effort',['../_contributions__page.html#shared_effort',1,'']]],
-  ['should_5faccept_5fmessage_3',['should_accept_message',['../class_distance_vector_router_1_1_distance_vector_router.html#af9233a362110d05b8ce67000a8f0a20b',1,'DistanceVectorRouter::DistanceVectorRouter']]],
-  ['should_5ftransmit_5fmessage_4',['should_transmit_message',['../class_distance_vector_router_1_1_distance_vector_router.html#a5fb0a0fcab6f9463a535ccda036aee6e',1,'DistanceVectorRouter::DistanceVectorRouter']]],
-  ['simulation_5',['ELEC 331 Programming Assignment 2 - Routing Protocols Simulation',['../index.html',1,'']]],
-  ['state_20routing_6',['Link State Routing',['../group___l_s_r.html',1,'']]]
+  ['sh_2',['make.sh',['../index.html#make_bash',1,'']]],
+  ['shared_20effort_3',['Shared Effort',['../_contributions__page.html#shared_effort',1,'']]],
+  ['should_5faccept_5fmessage_4',['should_accept_message',['../class_distance_vector_router_1_1_distance_vector_router.html#af9233a362110d05b8ce67000a8f0a20b',1,'DistanceVectorRouter::DistanceVectorRouter']]],
+  ['should_5ftransmit_5fmessage_5',['should_transmit_message',['../class_distance_vector_router_1_1_distance_vector_router.html#a5fb0a0fcab6f9463a535ccda036aee6e',1,'DistanceVectorRouter::DistanceVectorRouter']]],
+  ['simulation_6',['ELEC 331 Programming Assignment 2 - Routing Protocols Simulation',['../index.html',1,'']]],
+  ['state_20routing_7',['Link State Routing',['../group___l_s_r.html',1,'']]]
 ];
